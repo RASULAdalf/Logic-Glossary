c998ce6899bbc4bc137ded8b731d2c6dae2be11d
RASULAdalf <geethaniamaradiwakara@gmail.com>
2023-06-09 18:10:48 +0800

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Params} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
+import {FormControl, FormGroup} from "@angular/forms";
+import {debounceTime} from "rxjs";
+import {AppServiceService} from "./app-service.service";
+
 
 @Component({
   selector: 'app-root',
@@ -8,9 +12,23 @@ import {ActivatedRoute, Params} from "@angular/router";
 })
 export class AppComponent implements OnInit {
   title = 'Logic-Glossary';
+  fromLang = 'Sinhala';
+  toLang = 'English';
+
+  inputForm = new FormGroup({
+    inputText:new FormControl('')
+  });
+  outputForm = new FormGroup({
+    outputText:new FormControl('')
+  });
+
+  //Listeners
 
 
-  constructor(private activatedRoute: ActivatedRoute) {
+  constructor(private activatedRoute: ActivatedRoute,private router:Router,public appService:AppServiceService) {
+    this.inputForm.valueChanges.pipe(debounceTime(1080)).subscribe(inData=>{
+      this.router.navigate(['/'],{queryParams:{'word':inData.inputText,'fromLang':this.fromLang,'toLang':this.toLang}})
+    })
   }
 
   ngOnInit() {
@@ -22,4 +40,12 @@ export class AppComponent implements OnInit {
   }
 
 
+  swapLanguages(inputLang: any, outputLang: any) {
+    let prevInputLang = inputLang.textLabel;
+    inputLang.textLabel = outputLang.textLabel;
+    outputLang.textLabel = prevInputLang;
+
+    this.fromLang = inputLang.textLabel;
+    this.toLang = outputLang.textLabel;
+  }
 }
