2990e19fe97719fd14a38c8ab00db4cb73db58fa
RASULAdalf <geethaniamaradiwakara@gmail.com>
2023-06-12 00:00:06 +0800

    Interface completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -48,4 +48,30 @@ export class AppComponent implements OnInit {
     this.fromLang = inputLang.textLabel;
     this.toLang = outputLang.textLabel;
   }
+
+  recordAudioAndSend() {
+    navigator.mediaDevices.getUserMedia({ audio: true })
+      .then(stream => {
+        const mediaRecorder = new MediaRecorder(stream);
+        mediaRecorder.start();
+
+        // @ts-ignore
+        const audioChunks = [];
+        mediaRecorder.addEventListener("dataavailable", event => {
+          audioChunks.push(event.data);
+        });
+
+        mediaRecorder.addEventListener("stop", () => {
+          // @ts-ignore
+          const audioBlob = new Blob(audioChunks);
+          const audioUrl = URL.createObjectURL(audioBlob);
+          const audio = new Audio(audioUrl);
+          audio.play();
+        });
+
+        setTimeout(() => {
+          mediaRecorder.stop();
+        }, 3000);
+      });
+  }
 }
