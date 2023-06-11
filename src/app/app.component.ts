15e3d65d03fa20a0bcb10e563090c64d545cdc1c
RASULAdalf <92834629+RASULAdalf@users.noreply.github.com>
2023-06-12 00:24:10 +0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {FormControl, FormGroup} from "@angular/forms";
+import {debounceTime} from "rxjs";
+import {AppServiceService} from "./app-service.service";
 
 
 @Component({
@@ -10,7 +12,8 @@ import {FormControl, FormGroup} from "@angular/forms";
 })
 export class AppComponent implements OnInit {
   title = 'Logic-Glossary';
-
+  fromLang = 'Sinhala';
+  toLang = 'English';
 
   inputForm = new FormGroup({
     inputText:new FormControl('')
@@ -19,18 +22,30 @@ export class AppComponent implements OnInit {
     outputText:new FormControl('')
   });
 
+  //Listeners
 
 
-  constructor(private activatedRoute: ActivatedRoute) {
+  constructor(private activatedRoute: ActivatedRoute,private router:Router,public appService:AppServiceService) {
+    this.inputForm.valueChanges.pipe(debounceTime(1080)).subscribe(inData=>{
+      this.router.navigate(['/'],{queryParams:{'word':inData.inputText,'fromLang':this.fromLang,'toLang':this.toLang}})
+    })
   }
 
   ngOnInit() {
     this.activatedRoute.queryParams.subscribe(resp=>{
       if (resp['word']!= undefined && resp['fromLang']!=undefined && resp['toLang']!=undefined) {
-        console.log(resp['word'])
+        console.log(resp['word']);
       }
     })
   }
 
 
+  swapLanguages(inputLang: any, outputLang: any) {
+    let prevInputLang = inputLang.textLabel;
+    inputLang.textLabel = outputLang.textLabel;
+    outputLang.textLabel = prevInputLang;
+
+    this.fromLang = inputLang.textLabel;
+    this.toLang = outputLang.textLabel;
+  }
 }
